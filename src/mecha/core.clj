d04428429c94226db24a0cdd70cbc52defbe6c47
justinvdm <justinvdm.0.1.0@gmail.com>
2014-04-20 19:24:52 +0200

    basic arg parsing for defmecha

--- src/mecha/core.clj
+++ src/mecha/core.clj
@@ -1,1 +1,23 @@
 (ns mecha.core)
+
+
+(defmacro defmecha
+  "Define a thing capable of being started and stopped"
+  [m-name & args]
+  (let [[m-args m-state m-body]
+        (cond
+          (and (-> args first vector?)
+               (-> args second vector?))
+          args
+
+          (and (-> args first vector?)
+               (-> args second vector? not))
+          [(first args) [] (rest args)]
+
+          (and (-> args first vector? not)
+               (-> args second vector? not))
+          (concat [[] []] args))]
+    (println m-args m-state m-body)))
+
+
+(defn stop [])
