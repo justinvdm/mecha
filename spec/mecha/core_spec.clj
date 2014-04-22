7f54a67fdf94586850f08e3ff64c7724aca5f3e6
justinvdm <justinvdm.0.1.0@gmail.com>
2014-04-22 07:49:45 +0200

    failing tests for a new mecha

--- spec/mecha/core_spec.clj
+++ spec/mecha/core_spec.clj
@@ -3,34 +3,46 @@
             [mecha.core :refer :all]))
 
 
-(defmecha mecha-foo
-  (:start (println "a")
-          (println "r"))
-  (:stop (println "b")))
-
-
-(defmecha mecha-bar [a b]
-  (:start (println "c"))
-  (:stop (println "d")))
-
-
-(defmecha mecha-baz
-  (:start [foo (mecha-foo)
-           bar (mecha-bar)])
-  (:stop (:name foo)))
-
-
-(defmecha mecha-qux
-  (:start [foo (mecha-foo)
-           bar (mecha-bar)]))
-
-
 (describe "mecha"
-  (it "should be startable")
-  (it "should be stoppable")
-  (it "should start its own mecha when it is started")
-  (it "should stop its own mecha when it is stopped")
-  (it "should not mandate state")
-  (it "should not mandate args")
-  (it "should not mandate a start block")
-  (it "should not mandate a stop block"))
+  (with-stubs)
+
+  (it "should be startable"
+    (let [starter (stub :starter)
+          m (mecha (:start (starter)))]
+      (m)
+      (should-have-invoked :starter {:times 1})))
+
+  (it "should be stoppable"
+    (let [stopper (stub :stopper)
+          m (mecha (:stop (stopper)))]
+      (stop (m))
+      (should-have-invoked :stopper {:times 1})))
+
+  (it "should start its own mecha when it is started"
+    (let [starter (stub :starter)
+          m (mecha (:start [mecha (:start (starter))]))]
+      (m)
+      (should-have-invoked :starter {:times 1})))
+
+  (it "should stop its own mecha when it is stopped"
+    (let [stopper (stub :stopper)
+          m (mecha (:start [foo 23
+                            mecha (:stop (stopper))]))]
+      (stop (m))
+      (should-have-invoked :stopper {:times 1})))
+
+  (it "should not support args"
+    (let [starter (stub :starter)
+          m (mecha [a b] (:start (starter a b)))]
+      (m 1 2)
+      (should-have-invoked :starter {:times 1
+                                     :with [1 2]})))
+
+  (it "should handle state as a `let` would"
+    (let [starter (stub :starter)
+          m (mecha (:start [foo 1
+                            foo (inc foo)]
+                           (starter foo)))]
+      (m)
+      (should-have-invoked :starter {:times 1
+                                     :with [2]}))))
