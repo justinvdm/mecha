d9570d86002e38529f2b51bbefd8688783f09585
justinvdm <justinvdm.0.1.0@gmail.com>
2014-04-29 00:05:35 +0200

    bump version: 0.4.2 -> 0.5.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mecha "0.4.2"
+(defproject mecha "0.5.0"
   :description "coordinate starting and stopping things in clojure"
   :url "https://github.com/justinvdm/mecha"
   :license {:name "MIT"}
